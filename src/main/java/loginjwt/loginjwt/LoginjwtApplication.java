32c51c1f3f9987ba6652ed05ec3fd75f2c788280
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-07-30 23:27:38 -0300

    primeiro commit

---
+++ src/main/java/loginjwt/loginjwt/LoginjwtApplication.java
@@ -0,0 +1,13 @@
+package loginjwt.loginjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loginjwt/loginjwt/LoginjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package loginjwt.loginjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
