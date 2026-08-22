91169982f8d23104bba9c335cb0b7146cc0beec5
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-08-21 22:54:06 -0300

    Alterações para resoluções de problemas

--- src/main/java/loginjwt/loginjwt/Repository/LoginRepository.java
+++ src/main/java/loginjwt/loginjwt/Repository/LoginRepository.java
@@ -1,7 +1,10 @@
 package loginjwt.loginjwt.Repository;
 
 import loginjwt.loginjwt.Model.LoginEntity;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepository extends JpaRepository<LoginEntity,Long> {
+    static void save(User user) {
+    }
 }
--- src/main/java/loginjwt/loginjwt/Service/LoginService.java
+++ src/main/java/loginjwt/loginjwt/Service/LoginService.java
@@ -1,8 +1,13 @@
 package loginjwt.loginjwt.Service;
 
 import loginjwt.loginjwt.Dto.LoginResponse;
+import loginjwt.loginjwt.Repository.LoginRepository;
 import loginjwt.loginjwt.Security.JwtCofing.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.Group;
+import org.apache.catalina.Role;
+import org.apache.catalina.User;
+import org.apache.catalina.UserDatabase;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,13 +15,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
+    private final LoginRepository repository;
     private final PasswordEncoder passwordEncoder;
 
     public <LoginRequest> LoginResponse login(LoginRequest request) {
@@ -37,16 +44,121 @@ public class AuthService {
         return new LoginResponse(token);
     }
 
-    public void register(RegisterRequest request) {
-        if (userRepository.existsByEmail(request.email())) {
+    public <RegisterRequest> void register(RegisterRequest request) {
+        if (LoginRepository.save(request.email())) {
             throw new RuntimeException("Email já cadastrado");
         }
 
-        User user = new User();
-        user.setEmail(request.email());
-        user.setPassword(passwordEncoder.encode(request.password()));
-        user.setRole(Role.USER);
+        User user = new User() {
+            @Override
+            public boolean equals(Object another) {
+                return false;
+            }
 
-        userRepository.save(user);
+            @Override
+            public String toString() {
+                return "";
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+
+            @Override
+            public String getName() {
+                return "";
+            }
+
+            @Override
+            public String getFullName() {
+                return "";
+            }
+
+            @Override
+            public void setFullName(String s) {
+
+            }
+
+            @Override
+            public Iterator<Group> getGroups() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return "";
+            }
+
+            @Override
+            public void setPassword(String s) {
+
+            }
+
+            @Override
+            public Iterator<Role> getRoles() {
+                return null;
+            }
+
+            @Override
+            public UserDatabase getUserDatabase() {
+                return null;
+            }
+
+            @Override
+            public String getUsername() {
+                return "";
+            }
+
+            @Override
+            public void setUsername(String s) {
+
+            }
+
+            @Override
+            public void addGroup(Group group) {
+
+            }
+
+            @Override
+            public void addRole(Role role) {
+
+            }
+
+            @Override
+            public boolean isInGroup(Group group) {
+                return false;
+            }
+
+            @Override
+            public boolean isInRole(Role role) {
+                return false;
+            }
+
+            @Override
+            public void removeGroup(Group group) {
+
+            }
+
+            @Override
+            public void removeGroups() {
+
+            }
+
+            @Override
+            public void removeRole(Role role) {
+
+            }
+
+            @Override
+            public void removeRoles() {
+
+            }
+        };
+        user.getName(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.getRoles(Role.USER);
+
+        LoginRepository.save(user);
     }
 }
