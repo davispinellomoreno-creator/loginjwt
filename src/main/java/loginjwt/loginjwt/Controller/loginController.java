bbd4634d11bdab9c6e3c03f6340aaa3170c7a3a8
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-09-19 23:24:05 -0300

    Controller finalizado

---
+++ src/main/java/loginjwt/loginjwt/Controller/loginController.java
@@ -0,0 +1,30 @@
+package loginjwt.loginjwt.Controller;
+
+import jakarta.validation.Valid;
+import loginjwt.loginjwt.Dto.LoginRequest;
+import loginjwt.loginjwt.Dto.LoginResponse;
+import loginjwt.loginjwt.Dto.RegisterRequest;
+import loginjwt.loginjwt.Service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class loginController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponse> register(@Valid @RequestBody RegisterRequest request) {
+        LoginResponse response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
