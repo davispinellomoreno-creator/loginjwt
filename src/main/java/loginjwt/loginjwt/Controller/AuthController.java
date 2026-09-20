58ddf93de069f4e3aa0d16cfc0883f898e8ea831
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-09-19 23:31:14 -0300

    Classe controller renomeada

--- src/main/java/loginjwt/loginjwt/Controller/AuthController.java
+++ src/main/java/loginjwt/loginjwt/Controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class loginController {
+public class AuthController {
 
     private final AuthService authService;
 
